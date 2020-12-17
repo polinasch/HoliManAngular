2cc82002bc8548cea346ba19e3451a81fe784e9b
Polina <polina_sch@web.de>
2020-12-17 18:51:26 +0100

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BenutzerComponent } from './benutzer/benutzer.component';
+import { BenutzerverwaltungComponent } from './benutzerverwaltung/benutzerverwaltung.component';
+import { EditBenutzerComponent } from './edit-benutzer/edit-benutzer.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'benutzerverwaltung', component: BenutzerverwaltungComponent },
+  { path: 'benutzer', component: BenutzerComponent},
+  { path: 'editBenutzer/:id', component: EditBenutzerComponent}
 ];
 
 @NgModule({
