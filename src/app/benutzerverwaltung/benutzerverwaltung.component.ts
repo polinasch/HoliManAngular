9df760d618de8ed004018ee48c77f1c8a9dbb0ae
Polina <polina_sch@web.de>
2020-12-17 18:52:57 +0100

    benutzerverwaltung component

---
+++ src/app/benutzerverwaltung/benutzerverwaltung.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ITdDataTableColumn } from '@covalent/core/data-table';
+import { ApiModel } from '../Models/api.model';
+import { BenutzerService } from '../Services/benutzer.service';
+
+@Component({
+  selector: 'app-benutzerverwaltung',
+  templateUrl: './benutzerverwaltung.component.html',
+  styleUrls: ['./benutzerverwaltung.component.css']
+})
+export class BenutzerverwaltungComponent implements OnInit {
+  Benutzer: ApiModel.Benutzer[];
+
+  columns: ITdDataTableColumn[] = [
+    { name: 'BenutzerID', label: 'Benutzer ID', sortable: true },
+    { name: 'Benutzername', label: 'Benutzername', sortable: true },
+    { name: 'Vorname', label: 'Vorname', sortable: true },
+    { name: 'Nachname', label: 'Nachname', sortable: true },
+    { name: 'Email', label: 'Email', sortable: false },
+    { name: 'istVorgesetzter', label: 'Vorgesetzter', sortable: false },
+    { name: 'istAdmin', label: 'Admin', sortable: false },
+    { name: 'Aktionen', label: 'Aktionen', sortable: false }
+  ];
+
+  constructor(private benutzerService: BenutzerService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.getBenutzer();
+  }
+
+  getBenutzer(): void {
+    this.benutzerService.getBenutzer().subscribe(Benutzer => (this.Benutzer = Benutzer));
+  }
+
+}
